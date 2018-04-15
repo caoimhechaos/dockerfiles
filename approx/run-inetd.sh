5f6780eeb60ae4332fd6719db83be9aa3710018f
Caoimhe Chaos <caoimhechaos@protonmail.com>
2018-04-15 03:12:59 +0200

    Update environment variable name for approx listen port.
    APPROX_PORT is already taken by Kubernetes.

--- approx/run-inetd.sh
+++ approx/run-inetd.sh
@@ -2,5 +2,5 @@
 /usr/bin/install -o approx -g approx -m 0755 -d /srv/approx/cache
 /usr/sbin/rsyslogd
 usermod -s /bin/bash "$SETUID_USER"
-echo "$APPROX_PORT stream tcp nowait $SETUID_USER /usr/sbin/approx" > /etc/inetd.conf
+echo "$APPROX_LISTEN_PORT stream tcp nowait $SETUID_USER /usr/sbin/approx" > /etc/inetd.conf
 exec su "$SETUID_USER" -c "exec /usr/sbin/inetd -i /etc/inetd.conf"
