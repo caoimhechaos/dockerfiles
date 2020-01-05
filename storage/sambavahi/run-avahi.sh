0c0a47d7d1ae03218429df11ee1034061cec0423
Caoimhe Chaos <caoimhechaos@protonmail.com>
2020-01-05 02:40:35 +0100

    Create avahi services per share.

--- storage/sambavahi/run-avahi.sh
+++ storage/sambavahi/run-avahi.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
 
+if test x"$SERVICE_NAME" = x
+then
+	sed -i -e "s/@HOSTNAME@/%h/g" /etc/avahi/timemachine.service.in
+else
+	sed -i -e "s/@HOSTNAME@/${SERVICE_NAME}/g" /etc/avahi/timemachine.service.in
+fi
+
+for dir in /export/tm-*
+do
+	bn=`basename "$dir"`
+	cat /etc/avahi/timemachine.service.in | sed -e "s/@VOLUME@/${bn}/g" > \
+		/etc/avahi/services/timemachine-${bn}.service
+done
+
 dbus-daemon --system --fork
 exec /usr/bin/dumb-init /usr/sbin/avahi-daemon
